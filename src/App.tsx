eededfeab14dce9e84b7069434f835ea67fbafa3
rektDev <rektdevvv@gmail.com>
2022-08-31 20:38:37 +0300

    divider

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ export default function App() {
 
   return (
     <div className="divider">
-      <div className="divider">
+      <div className="">
         <Header search={getData} />
         <div></div>
 
